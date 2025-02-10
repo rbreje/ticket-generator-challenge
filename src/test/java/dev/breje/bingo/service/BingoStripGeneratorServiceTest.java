56ebe7878a8606b44a93345cc401edf631cd6339
Raul Breje <raulb@altapay.com>
2025-02-10 19:55:23 +0100

    Added test to assess performance

--- src/test/java/dev/breje/bingo/service/BingoStripGeneratorServiceTest.java
+++ src/test/java/dev/breje/bingo/service/BingoStripGeneratorServiceTest.java
@@ -4,6 +4,10 @@ import dev.breje.bingo.model.Bingo90Strip;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class BingoStripGeneratorServiceTest {
 
     private BingoStripGeneratorService bingoStripGeneratorService;
@@ -18,6 +22,18 @@ public class BingoStripGeneratorServiceTest {
         Bingo90Strip bingo90Strip = bingoStripGeneratorService.generateBingo90Strip();
 
         System.out.println(bingo90Strip);
+    }
+
+    @Test
+    public void testGenerateBingo90StripByExecutionTime() {
+        long startTime = System.nanoTime();
+
+        IntStream.range(0, 10000).forEach(index -> bingoStripGeneratorService.generateBingo90Strip());
+
+        long endTime = System.nanoTime();
+        double duration = (endTime - startTime) / 1_000_000_000.0;
+        System.out.println("Execution time: " + duration + " seconds");
+        assertTrue(duration < 1); // 1 second
     }
 
 }
